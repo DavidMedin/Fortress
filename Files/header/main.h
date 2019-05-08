c1dd56d3e123339fb740fa1b7d9eb815718522ed
DavidMedin <45665232+DavidMedin@users.noreply.github.com>
2019-05-07 22:49:26 -0500

    good luck meshing these

--- Files/header/main.h
+++ Files/header/main.h
@@ -7,9 +7,11 @@
 #include "list.h"
 #include "map.h"
 
+SDL_DisplayMode DM;
 
 Obj* objList; //might need to add a 'render this' bool to obj tho
 texture* loadedTextures; // doesn't include the map textures
+texture* mapLoadTexes;
 SDL_Renderer* renderer;
 SDL_Window* window;
 map* hub;
--- Files/src/main.cpp
+++ Files/src/main.cpp
@@ -13,9 +13,10 @@ int main(int argc, char* argv[]) {
 	IMG_Init(IMG_INIT_PNG);
 	window = SDL_CreateWindow("Fortress", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, 0);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	
+	SDL_GetCurrentDisplayMode(0, &DM);
 
 	loadedTextures = nullptr;
+	mapLoadTexes = nullptr;
 	hub = new map("../Data/map.map");
 	targetMap = hub;
 	/*room* roomList = nullptr;
@@ -41,22 +42,45 @@ int main(int argc, char* argv[]) {
 }
 
 void RenderWindow() {	
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 225);
-	SDL_RenderClear(renderer);
-	
-	//map rendering
-	SDL_Rect tmpRect;
 	Room* roomItr = targetMap->roomList;
 	do {
 		Tile* tileItr = roomItr->tileList;
 		do {
-			tmpRect = tileItr->rect;
-			tmpRect.w = tileItr->rect.w 
+			//add conditional that if tile is within the 'camera' view, then add to list
+			texture* mapTexItr = mapLoadTexes;
+			do {
+				
+				//maybe make a function for populating a list of unique items of a specific type
+
+				mapTexItr = mapTexItr->next;
+			} while (mapTexItr != nullptr);
 			tileItr = (Tile*)tileItr->next;
 		} while (tileItr != nullptr);
 		roomItr = roomItr->next;
 	} while (roomItr != nullptr);
-
+	
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 225);
+	SDL_RenderClear(renderer);
+	
+	//map rendering
+	// w / roomW * ScreenWidth
+	SDL_Rect tmpRect;
+	roomItr = targetMap->roomList;
+	do {
+		Tile* tileItr = roomItr->tileList;
+		do {
+			tmpRect = tileItr->rect;
+			tmpRect.w = tileItr->rect.w / roomItr->width * DM.w;
+			tmpRect.h = tileItr->rect.h / roomItr->height * DM.h;
+			tmpRect.x = tileItr->rect.x;
+			tmpRect.y = tileItr->rect.y;
+			SDL_Texture* tmpTex = 
+			SDL_RenderCopy(renderer,/*texture*/, NULL, &tmpRect);
+			tileItr = (Tile*)tileItr->next;
+		} while (tileItr != nullptr);
+		roomItr = roomItr->next;
+	} while (roomItr != nullptr);
+	
 	SDL_RenderPresent(renderer);
 }
 
